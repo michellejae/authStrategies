af9f2dc274606fc431ef49e07a662dbd04a86ade
michelle moyer <moyer.mj@gmail.com>
2018-08-06 14:44:26 -0400

    minimal style

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require(`express`);
+const path = require(`path`);
 const handlebars = require(`express-handlebars`);
 const bodyParser = require(`body-parser`)
 const methodOverride = require(`method-override`);
@@ -7,6 +8,7 @@ const app = express();
 app.engine(`.hbs`, handlebars({defaultLayout: `main`, extname: `hbs`}))
 app.set(`view engine`, `.hbs`);
 //app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 const PORT = process.env.PORT || 4040;
@@ -15,6 +17,7 @@ const authRoute = require(`./routes/index.js`);
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(`/auth`, authRoute)
+
 
 app.get(`/`, (req, res) => {
   res.render('home')
