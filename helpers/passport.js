61926e485a2a02c738da17d2e1888450f68dbc8b
michelle moyer <moyer.mj@gmail.com>
2018-08-06 18:28:18 -0400

    set up users

--- helpers/passport.js
+++ helpers/passport.js
@@ -1,6 +1,7 @@
 const passport = require(`passport`);
 const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const CONFIG = require(`../config/config.js`)
+const CONFIG = require(`../config/config.js`);
+const User = require(`../models/user-model`);
 
 
 passport.use(new googleStrategy({
@@ -9,9 +10,18 @@ passport.use(new googleStrategy({
   clientID: CONFIG.auth1.clientID,
   clientSecret: CONFIG.auth1.clientSECRET
 }, (accessToken, refreshToken, profile, done) => {
-    //passport callback function
-  User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    return done(err, user);
+
+  User.findOne({googleId: profile.id}).then((currentUser) => {
+    if(currentUser){
+      console.log(`new is ${currentUser}`)
+    } else {
+      new User({
+        username: profile.displayName,
+        googleId: profile.id
+      }).save().then((newUser) => {
+        console.log(`new user created ${newUser}`)
+      });
+    }
   });
 })
 )
